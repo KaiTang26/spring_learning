11518a8ae8ecd30158f9ab9bbe82b71783120c83
Kai Tang <16kt16@queensu.ca>
2021-07-29 22:29:49 -0400

    create package

---
+++ src/com/springdemo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.springdemo;
+
+public class BaseballCoach {
+    public String getDailWorkout(){
+
+        return "Spend 30 minutes on batting practice";
+    }
+}
---
+++ src/com/springdemo/MyApp.java
@@ -0,0 +1,14 @@
+package com.springdemo;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+
+        BaseballCoach theCoach = new BaseballCoach();
+
+        System.out.println(theCoach.getDailWorkout());
+
+    }
+
+
+}
