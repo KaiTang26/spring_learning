55df4ff32576a6fc9ff303d013d5ceb8032854ba
Kai Tang <16kt16@queensu.ca>
2021-07-29 22:36:47 -0400

    up to 21

--- src/com/springdemo/BaseballCoach.java
+++ src/com/springdemo/BaseballCoach.java
@@ -1,8 +1,9 @@
 package com.springdemo;
 
-public class BaseballCoach {
-    public String getDailWorkout(){
+public class BaseballCoach implements Coach{
 
+    @Override
+    public String getDailWorkout(){
         return "Spend 30 minutes on batting practice";
     }
 }
---
+++ src/com/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.springdemo;
+
+public class TrackCoach implements Coach{
+
+
+    @Override
+    public String getDailWorkout() {
+        return "TrackCoach is here";
+    }
+}
